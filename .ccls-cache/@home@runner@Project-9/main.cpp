320a5caac693b19b766f75e7d61d4c3549f52aed
Edwin Kurjak <112413421+elkurjak@users.noreply.github.com>
2022-10-25 03:32:04 +0000

    for loops

--- .ccls-cache/@home@runner@Project-9/main.cpp
+++ .ccls-cache/@home@runner@Project-9/main.cpp
@@ -4,9 +4,24 @@
 using namespace std;
 
 int main() {
+//declare constants for my number of cols/rows
+  const int COLUMNS = 5;
+  const int ROWS = 5;
+  //input grades
+  int grades [ROWS] [COLUMNS] = {{1, 100,100,100,100}, {2,100,0,100,0}, {3,82,94,73,86}, {4,64,74,84,94}, {5,94,84,74,64}};
+//for loops
+for(int a = 0; a < 5; a++)
+  {
+    for(int b = 0; b < 5; b++)
+    {
+      cout << setw(5)<< grades[a][b] ;
+    }
+    cout << endl;
+  } 
 
-  const int
 
 
   
 }
+
+  
