8a565e5a554a01a4ac043150b381a6eec84fb72b
Edwin Kurjak <112413421+elkurjak@users.noreply.github.com>
2022-10-25 03:52:46 +0000

    Calculated Averages

--- .ccls-cache/@home@runner@Project-9/main.cpp
+++ .ccls-cache/@home@runner@Project-9/main.cpp
@@ -10,16 +10,17 @@ int main() {
   //input grades
   int grades [ROWS] [COLUMNS] = {{1, 100,100,100,100}, {2,100,0,100,0}, {3,82,94,73,86}, {4,64,74,84,94}, {5,94,84,74,64}};
 //for loops
-for(int a = 0; a < 5; a++)
-  {
-    for(int b = 0; b < 5; b++)
-    {
+for(int a = 0; a < 5; a++){
+    for(int b = 0; b < 5; b++) {
       cout << setw(5)<< grades[a][b] ;
-    }
-    cout << endl;
+      }
+    int average = (grades[a][1] + grades [a][2] + grades [a][3] + grades [a][4]) / 4;
+    cout << setw(5) <<average;
+    int wAverage = ((grades [a][1] * 0.2) + (grades [a][2] * 0.3) + (grades [a][3] * 0.3) + (grades [a][4] * 0.2)) /4;
+  cout << setw(5) <<wAverage;
+      cout << endl;
   } 
-
-
+  
 
   
 }
